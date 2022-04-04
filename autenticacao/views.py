0fc211885efe1ef8222e703ad5ff0d5b83268336
Deivison Silva <deivisondamiao01@outlook.com>
2022-04-04 16:34:09 -0300

    adds messages tags on templates

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def cadastrar(request):
@@ -12,21 +13,26 @@ def cadastrar(request):
         confirmar_senha = request.POST.get('confirm-password')
 
         if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
             return redirect('/auth/cadastrar')
 
         if len(username.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
             return redirect('/auth/cadastrar')
 
         user = User.objects.filter(username=username)
 
         if user.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usário com esse username')
             return redirect('/auth/cadastrar')
 
         try:
             user = User.objects.create_user(username=username, password=senha)
             user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso')
             return redirect('/auth/logar')
         except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
             return redirect('/auth/cadastrar')
 
 
