369225e0a586e66479a401cc2d64a8161984032a
Deivison Silva <deivisondamiao01@outlook.com>
2022-04-04 12:11:11 -0300

    creates function cadastrar and validate it  and also renames cadastro template to cadastar

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 def cadastrar(request):
-    return render(request, 'autenticacao/cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'autenticacao/cadastrar.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('password')
+        confirmar_senha = request.POST.get('confirm-password')
+
+        if not senha == confirmar_senha:
+            return redirect('/auth/cadastrar')
+
+        if len(username.strip()) == 0 or len(senha.strip()) == 0:
+            return redirect('/auth/cadastrar')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            return redirect('/auth/cadastrar')
+
+        try:
+            user = User.objects.create_user(username=username, password=senha)
+            user.save()
+            return redirect('/auth/logar')
+        except:
+            return redirect('/auth/cadastrar')
 
 
 def logar(request):
