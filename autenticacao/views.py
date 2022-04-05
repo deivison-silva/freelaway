06146fc1802c713ddd986cb0c57e9c2ac2516bdd
Deivison Silva <deivisondamiao01@outlook.com>
2022-04-04 21:08:06 -0300

    adds function logar and also update logar template

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.messages import constants
 
 
 def cadastrar(request):
     if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'autenticacao/cadastrar.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
@@ -13,28 +15,49 @@ def cadastrar(request):
         confirmar_senha = request.POST.get('confirm-password')
 
         if not senha == confirmar_senha:
-            messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
+            messages.add_message(request, constants.ERROR,
+                                 'As senhas não coincidem')
             return redirect('/auth/cadastrar')
 
         if len(username.strip()) == 0 or len(senha.strip()) == 0:
-            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            messages.add_message(request, constants.ERROR,
+                                 'Preencha todos os campos')
             return redirect('/auth/cadastrar')
 
         user = User.objects.filter(username=username)
 
         if user.exists():
-            messages.add_message(request, constants.ERROR, 'Já existe um usário com esse username')
+            messages.add_message(request, constants.ERROR,
+                                 'Já existe um usário com esse username')
             return redirect('/auth/cadastrar')
 
         try:
             user = User.objects.create_user(username=username, password=senha)
             user.save()
-            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso')
+            messages.add_message(request, constants.SUCCESS,
+                                 'Usuário criado com sucesso')
             return redirect('/auth/logar')
         except:
-            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            messages.add_message(request, constants.ERROR,
+                                 'Erro interno do sistema')
             return redirect('/auth/cadastrar')
 
 
 def logar(request):
-    return render(request, 'autenticacao/logar.html')
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'autenticacao/logar.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('password')
+
+        user = auth.authenticate(username=username, password=senha)
+
+        if not user:
+            messages.add_message(request, constants.ERROR,
+                                 'Usuário ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, user)
+            return redirect('/')
