cc145417e506722546b9d90dff2f61ff08e8262b
Deivison Silva <deivisondamiao01@outlook.com>
2022-04-06 11:28:48 -0300

    adds url for media and static files

--- freelaway/urls.py
+++ freelaway/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,3 +8,5 @@ urlpatterns = [
     path('auth/', include('autenticacao.urls')),
     path('jobs/', include('jobs.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
