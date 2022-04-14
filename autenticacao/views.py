ef198525e2f88791d7479ae4dfd496b8d8c9eaf7
Deivison Silva <deivisondamiao01@outlook.com>
2022-04-14 00:38:06 -0300

    adds perfil navigation and updates redirect function logar

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -60,7 +60,7 @@ def logar(request):
             return redirect('/auth/logar')
         else:
             auth.login(request, user)
-            return redirect('/')
+            return redirect('/jobs/encontrar_jobs')
 
 
 def sair(request):
