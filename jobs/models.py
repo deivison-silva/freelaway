a9418ea8b7a596e22b4018bb9740207a406e0ffc
Deivison Silva <deivisondamiao01@outlook.com>
2022-04-14 00:11:42 -0300

    updates job status and adds arquivo_final field

--- jobs/models.py
+++ jobs/models.py
@@ -27,7 +27,8 @@ class Job(models.Model):
     profissional = models.ForeignKey(
         User, on_delete=models.DO_NOTHING, null=True, blank=True)
     reservado = models.BooleanField(default=False)
-    status = models.CharField(max_length=2, default='AA')
+    status = models.CharField(max_length=2, choices=status_choices, default='C')
+    arquivo_final = models.FileField(null=True)
 
     def __str__(self) -> str:
         return self.titulo
